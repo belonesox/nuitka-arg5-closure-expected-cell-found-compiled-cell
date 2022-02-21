d998181bef4ac1e6563882dd503cc062db3e76e7
stas <stas-fomin@yandex.ru>
2022-02-21 15:58:14 +0300

    19.2

--- demo_build.sh
+++ demo_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 python -m pipenv run python -m nuitka --version
-python -m pipenv run python -m nuitka --show-scons --standalone --show-progress --plugin-enable=numpy --standalone --follow-imports test.py >build.log 2>&1
+python -m pipenv run python -m nuitka --standalone --show-progress --plugin-enable=numpy --follow-imports test.py >build.log 2>&1
 
